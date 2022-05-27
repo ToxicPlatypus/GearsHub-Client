900d104f75e271933e00cc2fa04a79ecfcc13d43
ToxicPlatypus <rafidrabbi123@gmail.com>
2022-05-27 12:32:33 +0600

    password email

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,35 +1,104 @@
 import React from "react";
 import google from "../../Assets/Image/search.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
+import { type } from "@testing-library/user-event/dist/type";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  if (user) {
+    console.log(user);
+  }
+
+  if (loading) {
+  }
+
+  if (error) {
+  }
   return (
     <div>
       <h1 className="text-3xl text-center uppercase">login</h1>
+
       <div class="form-control w-full max-w-xs mx-auto">
-        <label class="label">
-          <span class="label-text">Username</span>
-        </label>
+        <span class="label-text mb-2">Email</span>
         <input
-          type="text"
-          placeholder="username"
+          type="email"
+          placeholder="email"
           class="input input-bordered w-full max-w-xs"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Email is required",
+            },
+            pattern: {
+              value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: "Provide a valid Email",
+            },
+          })}
         />
-      </div>
-      <div class="form-control w-full max-w-xs mx-auto">
         <label class="label">
-          <span class="label-text">Password</span>
+          {errors.email?.type === "required" && (
+            <span className="label-text-alt text-red-500">
+              {errors.email.message}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="label-text-alt text-red-500">
+              {errors.email.message}
+            </span>
+          )}
         </label>
+      </div>
+
+      <div class="form-control w-full max-w-xs mx-auto">
+        <span class="label-text mb-2">Password</span>
         <input
           type="password"
           placeholder="password"
           class="input input-bordered w-full max-w-xs"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "Password is required",
+            },
+            minLength: {
+              value: 6,
+              message: "Must be 6 character",
+            },
+          })}
         />
+        <label class="label">
+          {errors.password?.type === "required" && (
+            <span className="label-text-alt text-red-500">
+              {errors.password.message}
+            </span>
+          )}
+          {errors.password?.type === "minLength" && (
+            <span className="label-text-alt text-red-500">
+              {errors.password.message}
+            </span>
+          )}
+        </label>
       </div>
+
       <button className="btn btn-sm my-4 btn-accent flex justify-center items-center mx-auto ">
         Login
       </button>
       <div class="divider mx-20 my-4">OR</div>
-      <button className="btn flex btn-primary justify-center items-center mx-auto">
+      <button
+        className="btn flex btn-primary justify-center items-center mx-auto"
+        onClick={() => signInWithGoogle()}
+      >
         {" "}
         <span>
           <img className="px-3" src={google} alt="" />
---
+++ src/firebase.init.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCjY2M1-n_6M8p8TB9xpJJZIeYrKzz8phQ",
+//   authDomain: "gearshub-4851b.firebaseapp.com",
+//   projectId: "gearshub-4851b",
+//   storageBucket: "gearshub-4851b.appspot.com",
+//   messagingSenderId: "27024628113",
+//   appId: "1:27024628113:web:b981dac1dcc0365b58a230",
+// };
+
+console.log(firebaseConfig.appId);
+console.log("bols");
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
